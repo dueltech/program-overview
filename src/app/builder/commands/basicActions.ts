c5fb75901be0e6dd6f5d1cc859c395f0c99a6c74
Georg Donner <georgdonner@posteo.de>
2019-11-29 16:13:51 +0100

    Upload/download json files for import/export

--- src/app/builder/commands/basicActions.ts
+++ src/app/builder/commands/basicActions.ts
@@ -11,13 +11,24 @@ export default [
         fonts: getFonts(),
       };
       const jsonContent = JSON.stringify(exported);
+      const fileHref = `data:'text/json;charset=utf-8,${encodeURIComponent(jsonContent)}`;
       editor.Modal.setTitle('Export JSON')
-        .setContent(`<textarea style="width:100%; height: 250px;">${jsonContent}</textarea>`)
+        .setContent(`
+        <textarea style="width:100%; height: 250px;">${jsonContent}</textarea>
+        <a href="${fileHref}" download="program-overview.json">Download file</a>
+        `)
         .open();
     },
   },
   {
     id: 'import-json',
+    importJson(editor, jsonData: string) {
+      const importObj = JSON.parse(jsonData);
+      editor.setComponents(importObj.components);
+      editor.setStyle(importObj.style);
+      setFonts(editor, importObj.fonts);
+      editor.Modal.close();
+    },
     run(editor) {
       const wrapper = document.createElement('div');
       wrapper.innerHTML = '<textarea id="import-json" style="width:100%; height: 250px;"></textarea>';
@@ -25,13 +36,25 @@ export default [
       importButton.innerText = 'Import';
       importButton.addEventListener('click', () => {
         const textarea = document.getElementById('import-json') as HTMLTextAreaElement;
-        const importObj = JSON.parse(textarea.value);
-        editor.setComponents(importObj.components);
-        editor.setStyle(importObj.style);
-        setFonts(editor, importObj.fonts);
-        editor.Modal.close();
+        this.importJson(editor, textarea.value);
+      });
+      const input = document.createElement('input');
+      input.type = 'file';
+      input.name = 'json-import';
+      input.accept = '.json';
+      input.addEventListener('change', () => {
+        if (input.files.length > 0) {
+          const file = input.files[0];
+          const reader = new FileReader();
+          reader.onload = () => {
+            this.importJson(editor, reader.result);
+          };
+          reader.readAsText(file);
+        }
       });
       wrapper.appendChild(importButton);
+      wrapper.insertAdjacentHTML('beforeend', '<span class="footer-text">or</span>');
+      wrapper.appendChild(input);
       editor.Modal.setTitle('Import JSON')
         .setContent(wrapper)
         .open();
