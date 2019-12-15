e192d16e65df308579df7ba69fc50b29547fbbe7
Georg Donner <georgdonner@posteo.de>
2019-12-15 17:48:21 +0100

    Add select element to choose the default font

--- src/app/builder/fontManager.ts
+++ src/app/builder/fontManager.ts
@@ -10,30 +10,63 @@ export function getFonts(): [Font] {
   return JSON.parse(localStorage.getItem(LOCALSTORAGE_TAG)) || [];
 }
 
-function removeFont(fontName: string) {
+// returns the declaration of the default font
+function getDefaultFont(editor): string {
+  const bodyRule = editor.CssComposer.getRule('body');
+  if (bodyRule && bodyRule.config && bodyRule.config.style) {
+    return bodyRule.config.style['font-family'];
+  }
+  return null;
+}
+
+function removeFont(editor, font: Font) {
   const fonts = getFonts();
-  const updatedFonts = fonts.filter(font => font.name !== fontName);
+  const updatedFonts = fonts.filter(f => f.name !== font.name);
   localStorage.setItem(LOCALSTORAGE_TAG, JSON.stringify(updatedFonts));
+  const defaultFont = getDefaultFont(editor);
+  if (defaultFont === font.declaration) {
+    editor.CssComposer.setRule('body', {});
+  }
+}
+
+function setDefaultFont(editor, fontDeclaration: string) {
+  editor.CssComposer.setRule('body', { 'font-family': fontDeclaration });
 }
 
 export function resetFonts(editor) {
   const fonts = getFonts();
   editor.Canvas.getDocument().head.innerHTML = '';
-  fonts.forEach(font => removeFont(font.name));
+  fonts.forEach(font => removeFont(editor, font));
   localStorage.removeItem(LOCALSTORAGE_TAG);
 }
 
-function createFontEl(fontName: string) {
+// font html element to be added to the #added-fonts div in editor
+function createFontEl(editor, font: Font) {
   const wrapper = document.createElement('div');
-  wrapper.innerHTML = `<span>${fontName}</span>`;
+  wrapper.innerHTML = `<span>${font.name}</span>`;
   const removeButton = document.createElement('div');
   removeButton.className = 'fa fa-trash';
   removeButton.addEventListener('click', () => {
-    removeFont(fontName);
+    removeFont(editor, font);
     wrapper.remove();
   });
   wrapper.appendChild(removeButton);
   return wrapper;
+}
+
+function updateDefaultFontSelect(editor) {
+  console.log('update it');
+  const selectEl = document.getElementById('default-font') as HTMLSelectElement;
+  const fontProperty = editor.StyleManager.getProperty('basicText', 'font-family');
+  const availableFonts = fontProperty.attributes.list;
+  const defaultFont = getDefaultFont(editor);
+  const options = availableFonts.map((font) => `
+    <option value="${font.value}"${font.value === defaultFont ? ' selected' : ''}>${font.name}</option>
+  `).join('\n');
+  selectEl.innerHTML = `<option value="">None</option>\n${options}`;
+  selectEl.addEventListener('input', () => {
+    setDefaultFont(editor, selectEl.value);
+  });
 }
 
 function loadFont(editor, font: Font) {
@@ -46,13 +79,14 @@ function loadFont(editor, font: Font) {
   fontProperty.addOption({ value: font.declaration, name: font.name });
   // add font to list of fonts in fonts container
   const fontsList = document.getElementById('added-fonts');
-  fontsList.appendChild(createFontEl(font.name));
+  fontsList.appendChild(createFontEl(editor, font));
 }
 
 export function addFont(editor, font: Font) {
   const updatedFonts = getFonts().concat([font]);
   localStorage.setItem(LOCALSTORAGE_TAG, JSON.stringify(updatedFonts));
   loadFont(editor, font);
+  updateDefaultFontSelect(editor);
 }
 
 export function loadFonts(editor) {
@@ -62,6 +96,7 @@ export function loadFonts(editor) {
   });
   const fonts = getFonts();
   fonts.forEach((font) => loadFont(editor, font));
+  updateDefaultFontSelect(editor);
 }
 
 export function setFonts(editor, fonts: [Font]) {
